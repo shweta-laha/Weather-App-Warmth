f826bbb84bb5ef8bf854f831a1abc39073e3288d
Soumyadeep Pradhan <74774470+soumyadeeppradhan@users.noreply.github.com>
2021-06-19 22:34:43 +0530

    Final Commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,86 +1,95 @@
 import React from "react";
-import {useState,useCallback} from 'react';
-import {Box, TextField, Button, makeStyles} from '@material-ui/core';
-import {getData} from '../service/api.js';
-import Information from './information.jsx'
+import { useState, useCallback } from "react";
+import { Box, TextField, Button, makeStyles } from "@material-ui/core";
+import { getData } from "../service/api.js";
+import Information from "./information.jsx";
 
-
-const useStyles= makeStyles({
-component:{
-  padding:20,
-  backgroundColor:'rgba(31, 100, 128)'
-},
-
-input:{
-color:'white',
-marginRight:15,
-marginLeft:15,  
+const useStyles = makeStyles({
+  component: {
+    padding: 20,
+    backgroundColor: "rgba(31, 100, 128)",
   },
-button:{
-  width:100,
-  height:40,
-  marginLeft:10,
-  marginTop:5,
- borderRadius:60,
-  backgroundColor:'rgba(41, 43, 46,0.6)'
-}
 
-})
+  input: {
+    color: "white",
+    marginRight: 15,
+    marginLeft: 15,
+  },
+  button: {
+    width: 100,
+    height: 40,
+    marginLeft: 10,
+    marginTop: 5,
+    borderRadius: 60,
+    backgroundColor: "rgba(41, 43, 46,0.6)",
+  },
+});
 
+const Form = () => {
+  const classes = useStyles();
+  const [data, setWeatherData] = useState();
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const handleClick = useCallback(() => {
+    const getWeather = async () => {
+      city &&
+        country &&
+        (await getData(city, country).then((response) => {
+          setWeatherData(response.data);
+        }));
+    };
+    getWeather();
+  }, [city, country]);
 
-const Form= ()=> {
-const classes=useStyles();
- const [data,setWeatherData]=useState();
- const [city, setCity]=useState('');
- const [country, setCountry]=useState('');
- const handleClick=useCallback(() => {
-  const getWeather = async () => {
-    city && country && await getData(city, country)
-      .then(response => {
-        setWeatherData(response.data);
-      });
-  }
-  getWeather();
-}, [city, country]);
+  const handleCityChange = (value) => {
+    setCity(value);
+  };
 
+  const handleCountryChange = (value) => {
+    setCountry(value);
+  };
 
-const handleCityChange=(value)=>{
-  setCity(value);
-}
+  return (
+    <React.Fragment>
+      <Box className={classes.component}>
+        <TextField
+          label="Enter Your City"
+          onChange={(e) => handleCityChange(e.target.value)}
+          inputProps={{ className: classes.input }}
+          InputLabelProps={{
+            style: {
+              color: "rgba(184, 191, 194)",
+              fontSize: "13",
+              paddingBottom: 5,
+            },
+          }}
+          className={classes.input}
+        />
+        <TextField
+          id="standard-basic"
+          label="Enter Your Country"
+          onChange={(e) => handleCountryChange(e.target.value)}
+          inputProps={{ className: classes.input }}
+          InputLabelProps={{
+            style: {
+              color: "rgba(184, 191, 194)",
+              fontSize: "13",
+              paddingBottom: 5,
+            },
+          }}
+        />
 
-const handleCountryChange=(value)=>{
-  setCountry(value);
-}
-
-
-        return (
-        <React.Fragment>
-            <Box className={classes.component}>
-            <TextField 
-             label="Enter Your City"
-            onChange={(e)=>handleCityChange(e.target.value)} 
-            inputProps={{className:classes.input}}
-            InputLabelProps={{
-              style: { color: 'rgba(184, 191, 194)' , fontSize:'13', paddingBottom:5},
-            }}
-             className={classes.input}
-             />
-            <TextField id="standard-basic" 
-            label="Enter Your Country"
-            onChange={(e)=>handleCountryChange(e.target.value)}   
-            inputProps={{className:classes.input}} 
-            InputLabelProps={{
-              style: { color: 'rgba(184, 191, 194)', fontSize:'13', paddingBottom:5 },
-            }}/>
-
-            <Button  
-            variant="outlined" 
-            onClick={()=>handleClick(true)}
-            className={classes.button}
-            style={{color: 'rgba(184, 191, 194)'}}>Search</Button>
-            </Box>
-            <Information data={data}/>
-        </React.Fragment>
-        )   
-    }
-    export default Form;
+        <Button
+          variant="outlined"
+          onClick={() => handleClick(true)}
+          className={classes.button}
+          style={{ color: "rgba(184, 191, 194)" }}
+        >
+          Search
+        </Button>
+      </Box>
+      <Information data={data} />
+    </React.Fragment>
+  );
+};
+export default Form;
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,68 +1,84 @@
-import React from 'react';
-import {Box, makeStyles } from  '@material-ui/core';
-import logo from '../image/wall3.jpeg';
-import Form from './Form.jsx';
-import Clock from 'react-live-clock';
+import React from "react";
+import { Box, makeStyles } from "@material-ui/core";
+import logo from "../image/wall3.jpeg";
+import Form from "./Form.jsx";
+import Clock from "react-live-clock";
 
+const useStyles = makeStyles({
+  component: {
+    height: "100vh",
+    display: "flex",
+    alignItems: "center",
+    width: "90%",
+    marginLeft: "5%",
+  },
+  leftContainer: {
+    backgroundImage: `url(${logo})`,
+    height: "95vh",
+    width: "60%",
+    backgroundSize: "cover",
+    borderRadius: "60px 0 0 60px",
+  },
 
-const useStyles=makeStyles({
-component:{
-height:'100vh',
-display:'flex',
-alignItems:'center',
-width:'90%',
-marginLeft:'5%'
+  rightContainer: {
+    background:
+      "linear-gradient(to right,rgba(11, 55, 71),  rgba(188, 241, 247))",
+    color: "black",
+    height: "95vh",
+    width: "80%",
+    fontSize: 30,
+  },
+});
 
-},
-    leftContainer:{
-        backgroundImage: `url(${logo})`,
-        height:'95vh',
-        width:'60%',
-        backgroundSize: 'cover',
-        borderRadius: '60px 0 0 60px',
-      
-    },
+const Weather = () => {
+  const classes = useStyles();
 
-    rightContainer:{
-        background:'linear-gradient(to right,rgba(11, 55, 71),  rgba(188, 241, 247))',
-      color:'black',
-        height:'95vh',
-        width:'80%',
-        fontSize:30
-        
+  return (
+    <Box className={classes.component}>
+      <Box
+        className={classes.leftContainer}
+        style={{ fontFamily: "Quicksand" }}
+      >
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <div
+          style={{
+            textAlign: "center",
+            fontSize: "2.8vw",
+            fontFamily: "Quicksand",
+            letterSpacing: 2,
+            paddingBottom: 2,
+            color: "white",
+          }}
+        >
+          {new Date().getDate()}{" "}
+          {new Date().toLocaleString("en-US", { month: "long" })},{" "}
+          {new Date().getFullYear()}
+          <br />
+          <Clock
+            style={{ fontSize: "1.8vw" }}
+            format={"HH:mm:ss"}
+            ticking={true}
+            timezone={"India"}
+          />
+        </div>
+      </Box>
 
-    }
-})
-
-const Weather= ()=> {
-const classes = useStyles();
-
-    return (
-        <Box className={classes.component}>
-
-          <Box className={classes.leftContainer} style={{   fontFamily:'Quicksand',}}>
-          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-          <div style={{
-              textAlign:'center',
-               fontSize:'2.8vw',
-               fontFamily:'Quicksand',
-               letterSpacing:2,
-               paddingBottom:2,
-               color:'white'
-              }}>
-
-{new Date().getDate()} {new Date().toLocaleString("en-US", { month: "long" })}, {new Date().getFullYear()}
-<br/>
-           <Clock  style={{fontSize:'1.8vw'}}
-           format={'HH:mm:ss'} ticking={true} timezone={'India'} /></div>
-           </Box>
-
-          <Box className={classes.rightContainer}>
-             <Form/>
-          </Box>
-
-        </Box>
-        
-    )
-}
+      <Box className={classes.rightContainer}>
+        <Form />
+      </Box>
+    </Box>
+  );
+};
 export default Weather;
